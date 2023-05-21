fe827c035c4e1d105cbfcc794b6bc8b63a621254
BrianLahr <BrianLahr@users.noreply.github.com>
2023-05-21 02:59:19 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="BrianLahr"
+original_name="sphinx_first_project"
+original_urlname="sphinx_first_project"
+original_description="Awesome sphinx_first_project created by BrianLahr"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv sphinx_first_project $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- sphinx_first_project/cli.py
+++ sphinx_first_project/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for project_name project.
+"""CLI interface for sphinx_first_project project.
 
 Be creative! do whatever you want!
 
@@ -12,7 +12,7 @@ Be creative! do whatever you want!
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m sphinx_first_project` and `$ sphinx_first_project `.
 
     This is your program's entry point.
 
